0ddf99cb2bc2db32e9b4caa4400ec847c8be7689
delena0702 <delena0702@gmail.com>
2023-09-22 22:59:05 +0900

    import 페이지 작업 완료

---
+++ WEB-INF/js/import.js
@@ -0,0 +1,8 @@
+function init() {
+    const button_import = document.getElementById('button-import');
+    button_import.addEventListener('click', () => {
+        const text = document.getElementById('input-puzzle').value;
+    });
+}
+
+init();
