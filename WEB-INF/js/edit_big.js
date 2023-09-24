141bcffcd222e34b3206ec1515090eb683751a90
delena0702 <delena0702@gmail.com>
2023-09-24 22:14:35 +0900

    파일 정리

--- WEB-INF/js/edit_big.js
+++
@@ -1,5 +0,0 @@
-function init() {
-    img_board = new BoardContext('img-board', Board.testtest(), ConfigValue.MODE_BIG_EDIT);
-}
-
-init();
--- WEB-INF/js/solve_small.js
+++
@@ -1,8 +0,0 @@
-function init() {
-    img_board = new BoardContext('img-board', Board.testtest(), ConfigValue.MODE_SMALL_SOLVE, {
-        small_x: 1,
-        small_y: 2
-    });
-}
-
-init();
